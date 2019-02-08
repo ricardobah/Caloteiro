9ebad3d1390b847118294a40591251961791e4e1
ricardobah <rickribeiro99@gmail.com>
2019-02-08 13:15:53 -0300

    home css, change datepicker to dropdown

--- src/home.js
+++ src/home.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {Table} from'./table.js'
-
-
+import {Dropdown,InputGroup,DropdownButton,FormControl,Button} from 'react-bootstrap'
+import './home.css'
 
 
 class Home extends Component{
@@ -17,10 +17,55 @@ class Home extends Component{
                 <br/>
                 <h2>Caloteiros</h2>
                 <br/>
-                <div className="form-group">
+                <div className="form-group forf">
                  
-                    <label>First check in:</label>
-                    <input id ='from1' type="text" className="form-control form-control-1 input-sm from" placeholder="CheckIn" /> 
+                <InputGroup className="mb-3 col-md-6 ">
+                    <DropdownButton 
+                    as={InputGroup.Prepend}
+                    variant="outline-secondary"
+                    title="Ano"
+                    id="input-group-dropdown-1"
+                    >
+                    <Dropdown.Item href="#">2018</Dropdown.Item>
+                    <Dropdown.Item href="#">2019</Dropdown.Item>
+                    <Dropdown.Item href="#">2020</Dropdown.Item>
+                   
+                    </DropdownButton>
+                <FormControl aria-describedby="basic-addon1"  placeholder="Selecione um Ano"/>
+                &nbsp;&nbsp;&nbsp;
+                <DropdownButton
+                    as={InputGroup.Prepend}
+                    variant="outline-secondary"
+                    title="Mês"
+                    id="input-group-dropdown-1"
+                    >
+                        <Dropdown.Item href="#/action-1">Janeiro</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Fevereiro</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Março</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1">Abril</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Maio</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Junho</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1">Julho</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Agosto</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Setembro</Dropdown.Item>
+                        <Dropdown.Item href="#/action-1">Outubro</Dropdown.Item>
+                        <Dropdown.Item href="#/action-2">Novembro</Dropdown.Item>
+                        <Dropdown.Item href="#/action-3">Dezembro</Dropdown.Item>
+                    </DropdownButton>
+                    <FormControl
+                    placeholder="Selecioneum Mês"
+                    
+                    aria-describedby="basic-addon1"
+                    />
+                    &nbsp;&nbsp;&nbsp;
+                    <Button>Buscar</Button>
+                </InputGroup>
+
+               
+
+ 
+
+              
                 </div>
                 <Table/>
             
