8397616158fd84fdf3d1aa0378c57baefeddcfbf
ricardobah <rickribeiro99@gmail.com>
2019-02-08 17:21:14 -0300

    tela de pagamento

--- src/pagamentos.js
+++ src/pagamentos.js
@@ -1,23 +1,91 @@
 import React,{Component} from 'react'
-
-
+import {Dropdown,InputGroup,DropdownButton,FormControl,Button} from 'react-bootstrap'
+import './switch.css'
 class Pagamento extends Component{
+    constructor(props){
+        super(props)
+    
+        this.state = {
+          isLoading: false
+        }
+    }
+    
 
-render(){
+    render(){
+        return(
 
-    return(
+            <div>
+                <br/>
+                    <h2>Receita Federal</h2>
+                <br/>
+            <form action="/home"> 
+            {/* lembrar do post e name dos inputs */}
+            <div className="divisao_input">
+                <label>Nome: &nbsp;</label>
+                <div className="form-group forf">
+                    <InputGroup className="mb-3 col-md-3 ">
+                        <FormControl aria-describedby="basic-addon1"  placeholder="Nome"/>
+                    </InputGroup>
+                </div>
+            </div>
+            <div className="divisao_input">
+            <label>Valor: &nbsp;</label>
+                <div className="form-group forf">
+                    <InputGroup className="mb-3 col-md-3 ">
+                        <FormControl aria-describedby="basic-addon1"  placeholder="Valor"/>
+                    </InputGroup>
+                </div>
+            </div>
+            <div className="divisao_input">
+                <label>Data: &nbsp;</label>
+                <div className="form-group forf">
+                    <InputGroup className="mb-3 col-md-4 ">
+                        <DropdownButton as={InputGroup.Prepend} variant="outline-secondary"  title="Ano" id="input-group-dropdown-1" >
+                            <Dropdown.Item  >2018</Dropdown.Item>
+                            <Dropdown.Item  >2019</Dropdown.Item>
+                        </DropdownButton>
+                        < FormControl aria-describedby="basic-addon1"  placeholder="{this.state.year}"/>
+                        &nbsp;&nbsp;&nbsp;
+                        <DropdownButton as={InputGroup.Prepend} variant="outline-secondary" title="Mês" id="input-group-dropdown-1">
+                            <Dropdown.Item  >Outubro</Dropdown.Item>
+                            <Dropdown.Item  >Novembro</Dropdown.Item>
+                            <Dropdown.Item  >Dezembro</Dropdown.Item>
+                        </DropdownButton>
+                        <FormControl placeholder="{this.state.month}" aria-describedby="basic-addon1" />
+                    
+                        
+                    </InputGroup>
+               </div>
+            </div>
+          
 
-        <h1>Receita Federal</h1>
+            <div className="divisao_input">
+                <label>Transferência: &nbsp;</label>
+                <div className="form-group forf">
+                    <label class="switch">
+                        <input type="checkbox"/>
+                        <span class="slider round"></span>
+                    </label>
+                </div>
+            </div>
+
+                <Button type="submit" className="btn-success">Pagar</Button>
+          
+                </form>
+
+        </div>
 
 
-    );
+        );
+    }
+
 
 
 }
 
 
 
-}
+
 
 
 export {Pagamento}
