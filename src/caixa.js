d81fcd8b85171b37c21f2e11c48d25c3f2701de1
ricardobah <rickribeiro99@gmail.com>
2019-02-08 18:16:00 -0300

    commit texto e div na caixa para ir embora

--- src/caixa.js
+++ src/caixa.js
@@ -19,8 +19,17 @@ class Caixa extends Component{
                     <h2>Caixa</h2>
                 <br/>
            
+                <div className="Content  forf col-12">
+                    <div className="col-6">
+                        <h4>Relatório</h4>
+                    </div>
+                    <div className="col-6">
+                        <h4>Gráfico</h4>
+                    </div>
+                </div>
 
-        </div>
+
+            </div>
 
 
         );
