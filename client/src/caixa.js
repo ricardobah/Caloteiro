d87c5c0d5e21fe533209e2014051981251a2aec9
ricardobah <rickribeiro99@gmail.com>
2019-02-18 17:04:51 -0300

    revertendo alterações

--- client/src/caixa.js
+++ client/src/caixa.js
@@ -46,11 +46,7 @@ class Caixa extends Component{
                         <h3><font color="red">Saidas: 85</font></h3>
                         <br/>
                         <h3><font color="green">Total: 15</font></h3>
-                        <br/><br/>
-                        <button className="btn btn-success">2018</button> &nbsp;
-                        <button className="btn btn-success">2019</button> &nbsp;
-                        <button className="btn btn-success">2020</button><br/><br/>
-                        <button className="btn btn-success">2018</button> &nbsp;
+                        <br/><br/><br/> <br/><br/><br/> <br/>
                     </div>
                     <div className="col-6">
                         <h3>Gráfico</h3>
